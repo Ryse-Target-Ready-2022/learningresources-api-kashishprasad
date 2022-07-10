8217f951133cfa773cefcadb14a9ff7c4fcdf2fc
KashishPrasad <kashishp1310@gmail.com>
2022-07-10 22:51:19 +0530

    HelloWorld committed

---
+++ learningresources-api-kashish/src/main/java/com/tgt/rysetii/learningresourcesapikashish/LearningresourcesApiKashishApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetii.learningresourcesapikashish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiKashishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiKashishApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ learningresources-api-kashish/src/test/java/com/tgt/rysetii/learningresourcesapikashish/LearningresourcesApiKashishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapikashish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiKashishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
